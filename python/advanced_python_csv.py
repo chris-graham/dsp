35b865bfde7c2009d9544e5fdd070b8d36b1df6c
Chris Graham <chris_graham@acm.org>
2016-03-14 17:21:07 -0400

    Committing solution to CSV challenge @reshama @aramnath

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,11 @@
-PLACE YOUR CODE HERE
+import csv
+from advanced_python_regex import Faculty
+
+# Create a new Faculty object and get the list of emails from the data file
+f = Faculty('faculty.csv')
+emails = f.get_emails()
+
+# Create emails.cvs and write each email as a row in the file
+with open ('emails.csv', 'wb') as csv_file:
+	email_writer = csv.writer(csv_file, delimiter='\n')
+	email_writer.writerows([emails])
