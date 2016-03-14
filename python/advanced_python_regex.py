7795b32cf92297ac73bd8036786d0ad2f7703f31
Chris Graham <chris_graham@acm.org>
2016-03-14 16:32:43 -0400

    Refactored code to use a class so I can reuse it to grab all the emails in the CVS challenge @reshama @aramnath

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,91 +1,91 @@
 import csv
 import re
 
-def read_data(file):
-	parsed_data = []
+class Faculty:
 
-	# Read in data
-	with open('faculty.csv') as csv_file:
-		reader = csv.DictReader(csv_file)
+	# Initialize class with parsed data file
+	def __init__(self, file):
+		self.parsed_data = []
 
-		for row in reader:
-			parsed_data.append(row)
+		with open(file) as csv_file:
+			reader = csv.reader(csv_file)
 
-	return parsed_data
+			for row in reader:
+				self.parsed_data.append(row)
 
+	def get_degrees(self):
+		degrees = []
+		degree_count = {}
 
-def get_degrees(parsed_data):
-	degrees = []
-	degree_count = {}
+		# Append each degree from the data to a list
+		for i in self.parsed_data:
+			degrees.append(i[1])
 
-	for i in parsed_data:
-		degrees.append(i[' degree'])
+		# Normalize degree data by removing all '.' and find all degrees that 
+		# match pattern
+		degrees = re.findall(r'[A-Za-z]+', (str(degrees).replace('.', '')))
 
-	degrees = re.findall(r'[A-Za-z]+', (str(degrees).replace('.', '')))
+		# Sum number of each degree
+		for i in degrees:
+			if i in degree_count:
+				degree_count[i] += 1
+			else:
+				degree_count[i] = 1
 
-	for i in degrees:
-		if i in degree_count:
-			degree_count[i] += 1
-		else:
-			degree_count[i] = 1
-
-	return degree_count
-
-
-def get_titles(parsed_data):
-	titles = []
-	title_count = {}
-
-	for i in parsed_data:
-		titles.append(i[' title'])
-
-	titles = re.findall(r'[A-Za-z]+\sProfessor|Professor', str(titles))
-
-	for i in titles:
-		if i in title_count:
-			title_count[i] += 1
-		else:
-			title_count[i] = 1
-
-	return title_count
+		return degree_count
 
 
-def get_emails(file):
-	parsed_data = []
-	emails = []
+	def get_titles(self):
+		titles = []
+		title_count = {}
 
-	# Read in data
-	with open('faculty.csv') as csv_file:
-		reader = csv.reader(csv_file)
+		# Append each title from the data to a list
+		for i in self.parsed_data:
+			titles.append(i[2])
 
-		for row in reader:
-			parsed_data.append(row)
+		# Find all titles that match pattern
+		titles = re.findall(r'[A-Za-z]+\sProfessor|Professor', str(titles))
 
-	print parsed_data
-	p = re.compile(r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', re.IGNORECASE)
-	str_data = (' '.join(i) for i in parsed_data)
+		# Sum number of each title
+		for i in titles:
+			if i in title_count:
+				title_count[i] += 1
+			else:
+				title_count[i] = 1
 
-	for i in str_data:
-		match = p.search(i)
-		if match:
-			emails.append(match.group(0))
-
-	return emails
-
-def get_domains(parsed_data):
-	emails = ' '.join(parsed_data)
-	domains = re.findall(r'@([A-Za-z0-9.-]+\.[A-Za-z]{2,})', emails)
-
-	return set(domains)
+		return title_count
 
 
-faculty = read_data('faculty.csv')
-print 'Question 1'
-print get_degrees(faculty)
-print '\nQuestion 2'
-print get_titles(faculty)
-print '\nQuestion 3'
-emails = get_emails('faculty.csv')
-print emails
-print '\nQuestion 4'
-print get_domains(emails)
+	def get_emails(self):
+		emails = []
+		# Compile regex as it will be used multiple times
+		p = re.compile(r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', re.IGNORECASE)
+		# Converts parsed data from a list of lists to a list of strings
+		str_data = (' '.join(i) for i in self.parsed_data)
+
+		# Find email address in each row of data and append to list
+		for i in str_data:
+			match = p.search(i)
+			if match:
+				emails.append(match.group(0))
+
+		return emails
+
+
+	def get_domains(self, emails):
+		# Converts list of emails into string of emails and find all domains
+		# that match pattern
+		domains = re.findall(r'@([A-Za-z0-9.-]+\.[A-Za-z]{2,})', ' '.join(emails))
+
+		# Identify unique domains by converting domain list to a set
+		return set(domains)
+
+
+# Testing class and functions
+f = Faculty('faculty.csv')
+emails = f.get_emails()
+
+print 'Question 1\n' + str(f.get_degrees())
+print '\nQuestion 2\n' + str(f.get_titles())
+print '\nQuestion 3\n' + str(emails)
+print '\nQuestion 4\n' + str(f.get_domains(emails))
